f350d4f07f91111f467b78831a6184277baf5935
pazobii <triallder@gmail.com>
2024-10-04 20:31:35 -0300

    aa

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\programas\\FLUTTER\\flutter_windows_3.24.0-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\pazobi\\Desktop\\LAB1_DISPOSITIVOSMOVILES\\laboratorio1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\programas\\FLUTTER\\flutter_windows_3.24.0-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\pazobi\\Desktop\\LAB1_DISPOSITIVOSMOVILES\\laboratorio1"
+  "FLUTTER_ROOT=D:\\programas\\FLUTTER\\flutter_windows_3.24.0-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\pazobi\\Desktop\\LAB1_DISPOSITIVOSMOVILES\\laboratorio1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\pazobi\\Desktop\\LAB1_DISPOSITIVOSMOVILES\\laboratorio1"
+  "FLUTTER_TARGET=C:\\Users\\pazobi\\Desktop\\LAB1_DISPOSITIVOSMOVILES\\laboratorio1\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\pazobi\\Desktop\\LAB1_DISPOSITIVOSMOVILES\\laboratorio1\\.dart_tool\\package_config.json"
+)
