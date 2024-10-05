722c80a8df3cfd96dfe027d17a3c04172e8de44e
pazobii <triallder@gmail.com>
2024-10-05 01:03:03 -0300

    laab5

--- lab5/lib/main.dart
+++ lab5/lib/main.dart
@@ -1,33 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main() 
+{
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget 
+{
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) 
+  {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -36,73 +23,72 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatefulWidget 
+{
+  final String title;
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() 
+  {
+    print('create state');
+    return _MyHomePageState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> 
+{
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+  @override
+  void initState() //Este método se llama cuando el widget se crea por primera vez. Acá se pueden inicializar los datos!!!
+  {
+    super.initState();
+    print('initState inicializado correctamente'); //Este mensaje se debería ver en la consola cuando se ejecute
+  }
+
+  @override
+  void didChangeDependencies() //Se usa cuando se cambian las dependencias de los widgets 
+  {
+    super.didChangeDependencies();
+    print('didChangeDependencies dependencias del widget cambiando');
+  }
+
+  @override
+  void didUpdateWidget(covariant MyHomePage oldWidget) 
+  {
+    super.didUpdateWidget(oldWidget);
+    print('didUpdateWidget: el widget fue actualizado');
+  }
+
+  @override
+  void reassemble() 
+  {
+    super.reassemble();
+    print('reassemble: la aplicación fue reconstruida durante un hot reload');
+  }
+
+  void _incrementCounter() //El método incrementa el valor del _counter 
+  {
+    setState(() //avisa q el widget cambió y debe reconstruirse
+    {
       _counter++;
+      print('setState incrementado el contadoooor $_counter'); // el setState causa que el widget se vuelva a construir
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build(BuildContext context) 
+  {
+    print('build: widget se está construyendo');
+    
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -115,11 +101,73 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 20), // Espacio entre botones
+          FloatingActionButton(
+            heroTag: null, // Esto desactiva la animación de Hero
+            onPressed: () 
+            {
+              Navigator.pop(context); // Volver a la página anterior
+            },
+            tooltip: 'Volver',
+            child: const Icon(Icons.arrow_back),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void deactivate() //Se llama cuando el widget está a punto de ser eliminado, como cuando se cambiaa de pantalla!!
+  {
+    print('deactivate: widget se desactiva cuando se cambia de pantalla');
+    super.deactivate();
+  }
+
+  @override
+  void dispose() //Acá se llama cuando el widget se va a eliminar permanentementee
+  {
+    print('dispose: widget eliminado');
+    super.dispose();
+  }
+}
+
+// Segunda pantalla
+class SegundaPantalla extends StatelessWidget //widget simple que muestra la segunda pantalla
+{
+  const SegundaPantalla({super.key});
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pantalla 2"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text("HOLAAAAAAAAA!!! esta es la segunda pantalla :3"),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        heroTag: null, // Esto desactiva la animación de Hero
+        onPressed: () 
+        {
+          Navigator.pop(context); // Volver a la página anterior
+        },
+        tooltip: 'Volver',
+        child: const Icon(Icons.arrow_back),
+      ),
     );
   }
 }
